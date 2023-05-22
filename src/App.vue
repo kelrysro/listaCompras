425c947a0273c880dc3566401a4737d5f10afe60
kelrysro <kelryrodrigues86@gmail.com>
2023-05-22 12:01:31 -0300

    text

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import {livros} from '@/_data/livros'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h1>Minha livraria</h1>
+  <div class="livro"> 
+    
+    <div class="card-livro" v-for="livro in livros" :key="livro.id">
+    <img :src="livro.cover" class="livro-capa">
+    <p class="livro-titulo">{{ livro.title }}</p>
+    <p class="livro-autor">{{ livro.author }}</p>
+    <p class="livro-preco">{{ livro.price }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/_data/livros.js
@@ -0,0 +1,30 @@
+export const livros = [
+    {
+        id: 1,
+        title: "o poderoso chefão",
+        author: "mario puzo",
+        price: 49.9,
+        cover:"https://m.media-amazon.com/images/I/71wy+cucv9L._AC_UL400_.jpg"
+      },
+      {
+        id: 2,
+        title: "O senhor dos Aneís",
+        author: "J.R.R.",
+        price: 49.9,
+        cover:"https://m.media-amazon.com/images/I/A16GGot7vHL._AC_UL400_.jpg"
+      },
+      {
+        id: 3,
+        title: "anne com e",
+        author: "Lucy Maud ",
+        price: 49.9,
+        cover:"https://m.media-amazon.com/images/I/81-co-k8J8L._AC_UL400_.jpg"
+      },
+      {
+        id: 4,
+        title: "Anjos das Sombras",
+        author: "Karllen Koen ",
+        price: 49.9,
+        cover:"https://m.media-amazon.com/images/I/91n6G5fVFhL._AC_UL400_.jpg"
+      },
+]
